52787be1cdbf49541efd26fb54ac89444b38aaa3
vj abishek <abigosearch@gmail.com>
2020-04-05 19:44:14 +0530

    Fixed some Bugs��

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
-console.log(process.env)
+
 export default function Login() {
   return (
     <div className='login'>
       <h1>Spotify Lyrics</h1>
-      <a
-        href={
-          process.env.NODE_ENV === 'development'
-            ? 'http://localhost:8000/login'
-            : process.env.REACT_APP_URL
-        }
-        className='button-login'
-      >
+      <a href='https://spotify-server.now.sh/login' className='button-login'>
         Log in to Spotify
       </a>
     </div>
--- src/Components/LyricsComp.tsx
+++ src/Components/LyricsComp.tsx
@@ -3,10 +3,9 @@ import queryString from 'query-string'
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
 
-console.log(process.env)
 export default function LyricsComp(props: any) {
   const [error, setError] = useState(false)
-  const [track, setTrack] = useState()
+  // const [track, setTrack] = useState()
   const [lyrics, setLyrics] = useState('Loading...')
   const [gotLyrics, setGotLyrics] = useState(false)
   const [songId, setId] = useState('15vzANxN8G9wWfwAJLLMCg')
@@ -14,97 +13,96 @@ export default function LyricsComp(props: any) {
 
   useEffect(() => {
     //
-    const fetchSong = () => {
-      axios
-        .get('https://api.spotify.com/v1/me/player/currently-playing', {
-          headers: {
-            Authorization: `Bearer ${localStorage.access_token}`
-          }
-        })
-        .then(data => {
-          if (data.status === 200) {
-            const da: any = data.data
-            // console.log(da)
-            if (da.is_playing) {
-              // console.log(data)
+    if (gotLyrics === false) {
+      const fetchSong = () => {
+        axios
+          .get('https://api.spotify.com/v1/me/player/currently-playing', {
+            headers: {
+              Authorization: `Bearer ${localStorage.access_token}`,
+            },
+          })
+          .then((data) => {
+            if (data.status === 200 && gotLyrics === false) {
+              const da: any = data.data
+              console.log(da)
+              if (da.is_playing) {
+                console.log(data)
 
-              const name: any = da.item.name
-              setSongName(name)
-              // console.log(name)
-              axios
-                .get(
-                  `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${name}&apikey=${process.env.REACT_APP_API_KEY}&page_size=3&page=1&s_track_rating=desc`
-                )
-                .then(data => {
-                  const da: any = data
-                  const id = data.data.message.body.track_list[0].track.track_id
+                const name: any = da.item.name
+                setSongName(name)
+                console.log(name)
+                axios
+                  .get(`https://spotify-server.now.sh/track/${name}`)
+                  .then((data) => {
+                    const da: any = data
+                    const id =
+                      data.data.message.body.track_list[0].track.track_id
 
-                  // console.log(id)
-                  axios
-                    .get(
-                      `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${process.env.REACT_APP_API_KEY}`
-                    )
-                    .then(data => {
-                      try {
-                        const da: any = data
-                        const lyrc: any =
-                          da.data.message.body.lyrics.lyrics_body
+                    console.log(id)
+                    axios
+                      .get(`https://spotify-server.now.sh/lyrics/${id}`)
+                      .then((data) => {
+                        try {
+                          const da: any = data
+                          const lyrc: any =
+                            da.data.message.body.lyrics.lyrics_body
 
-                        setLyrics(lyrc)
-                        setGotLyrics(true)
-                      } catch (err) {
-                        setError(true)
-                      }
-                    })
-                  setTrack(da)
-                  console.log(track)
-                })
-                .catch(err => {
-                  setError(true)
-                })
+                          setLyrics(lyrc)
+                          setGotLyrics(true)
+                        } catch (err) {
+                          setError(true)
+                        }
+                      })
+                    // setTrack(da)
+                  })
+                  .catch((err) => {
+                    setError(true)
+                  })
+              }
+            } else {
+              console.log('Im here heheh')
+              let dummy: any = 'Try to play the song'
+              setLyrics(dummy)
+              setGotLyrics(true)
             }
-          } else {
-            // console.log('Im here heheh')
-            let dummy: any = 'Try to play the song'
-            setLyrics(dummy)
-            setGotLyrics(true)
-          }
-        })
-        .catch(err => {
-          setError(true)
+          })
+          .catch((err) => {
+            setError(true)
 
-          axios
-            .get(
-              `${process.env.URL ||
-                'http://localhost:8000'}/refresh_token#refresh_token=${
-                localStorage.refresh_token
-              }`
-            )
-            .then(data => console.log(data))
-        })
-    }
+            axios
+              .get(
+                `${
+                  process.env.URL || 'http://localhost:8000'
+                }/refresh_token#refresh_token=${localStorage.refresh_token}`
+              )
+              .then((data) => console.log(data))
+          })
+      }
 
-    const parsedHash: any = queryString.parse(props.location.hash)
-    if (localStorage.access_token) {
-      // console.log('Im here')
-      fetchSong()
-      axios
-        .get(`https://api.spotify.com/v1/me/player/recently-played?limit=1`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.access_token}`
-          }
-        })
-        .then(data => {
-          let recentSong: any = data.data.items[0].track.id
-          // console.log(recentSong)
-          setId(recentSong)
-        })
+      const parsedHash: any = queryString.parse(props.location.hash)
+      if (localStorage.access_token) {
+        console.log('Im here')
+        fetchSong()
+        axios
+          .get(`https://api.spotify.com/v1/me/player/recently-played?limit=1`, {
+            headers: {
+              Authorization: `Bearer ${localStorage.access_token}`,
+            },
+          })
+          .then((data) => {
+            let recentSong: any = data.data.items[0].track.id
+            console.log(recentSong)
+            setId(recentSong)
+          })
+      } else {
+        localStorage.setItem('access_token', parsedHash.access_token)
+        localStorage.setItem('refresh_token', parsedHash.refresh_token)
+        fetchSong()
+      }
     } else {
-      localStorage.setItem('access_token', parsedHash.access_token)
-      localStorage.setItem('refresh_token', parsedHash.refresh_token)
-      fetchSong()
+      return
     }
-  }, [props.location.hash, track])
+  }, [props.location.hash, gotLyrics])
 
   return error ? (
     <h1 style={{ textAlign: 'center' }}>
@@ -113,7 +111,6 @@ export default function LyricsComp(props: any) {
     </h1>
   ) : (
     <div className='login' style={{ margin: '0 auto' }}>
-      {' '}
       {gotLyrics ? (
         <>
           <h2>{songName} : </h2>
@@ -125,7 +122,7 @@ export default function LyricsComp(props: any) {
                   margin: '30 auto',
                   fontSize: '26px',
                   padding: '5px',
-                  textAlign: 'center'
+                  textAlign: 'center',
                 }}
                 key={index}
               >
