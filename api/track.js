6cac7627cbb436d28122a48c61e61c2318817d42
vj-abishek <abigosearch@gmail.com>
2021-01-12 16:35:54 +0530

    Bug fixes

--- api/track.js
+++ api/track.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
 
     if (req.method === "GET") {
         //musixmatch URL
-        const name = req.que
+        const name = req.query
         const url = `https://api.musixmatch.com/ws/1.1/track.search?q_track=${name}&apikey=${process.env.API_KEY}&page_size=3&page=1&s_track_rating=desc`
         request.get(url, (err, resp, body) => {
             if (resp.statusCode === 200) {
