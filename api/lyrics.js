b4c017d5b161dbc429c9f799f36606883f43a6dd
vj-abishek <abigosearch@gmail.com>
2021-01-12 16:29:57 +0530

    Bug fixes

--- api/lyrics.js
+++ api/lyrics.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 const request = require('request')
 
 module.exports = (req, res) => {
-    const id = req.query
-    console.log(id)
-    const url = `http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${process.env.API_KEY}`
-    request(url, (err, resp, body) => {
-        if (resp.statusCode === 200) {
-            res.send(body)
-        }
-    })
+    if (req.method === "GET") {
+        const id = req.query
+        console.log(id)
+        const url = `http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${process.env.API_KEY}`
+        request(url, (err, resp, body) => {
+            if (resp.statusCode === 200) {
+                res.send(body)
+            }
+        })
+    }
+
 }
--- api/track.js
+++ api/track.js
@@ -3,12 +3,14 @@ require('dotenv').config()
 const request = require('request')
 module.exports = (req, res) => {
 
-    //musixmatch URL
-    const name = req.query
-    const url = `https://api.musixmatch.com/ws/1.1/track.search?q_track=${name}&apikey=${process.env.API_KEY}&page_size=3&page=1&s_track_rating=desc`
-    request.get(url, (err, resp, body) => {
-        if (resp.statusCode === 200) {
-            res.send(body)
-        }
-    })
+    if (req.method === "GET") {
+        //musixmatch URL
+        const name = req.que
+        const url = `https://api.musixmatch.com/ws/1.1/track.search?q_track=${name}&apikey=${process.env.API_KEY}&page_size=3&page=1&s_track_rating=desc`
+        request.get(url, (err, resp, body) => {
+            if (resp.statusCode === 200) {
+                res.send(body)
+            }
+        })
+    }
 }
